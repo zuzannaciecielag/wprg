c58e4978f2f6fbf4bb8b71aa5c62d8ee393bf7c6
s25093 <s25093@pjwstk.edu.pl>
2022-03-14 17:08:55 +0100

    zadania 2

--- zadania2.php
+++ zadania2.php
@@ -46,3 +46,68 @@ function censorship($sentence) {
 
 $censor = censorship("fuck this");
 echo $censor;
+
+echo "<br />";
+echo "Zadanie 4";
+echo "<br />";
+
+//032[80 jesli 1800-1899, 0 jesli 1900-1999, 20 jesli po 2000] 218 0872 9
+
+function peselBirthDate($pesel) {
+    $arr = str_split($pesel);
+    $birthDate = "";
+    for($i = 4; $i < 6; $i++) {
+        $temp = (string) $arr[$i];
+        $birthDate .= $temp;
+    }
+    $birthDate .= "-";
+
+    if($arr[2] == 2) {
+        $birthDate .= "0";
+    }
+    if($arr[2] == 3) {
+        $birthDate .= "1";
+    }
+    $temp = (string) $arr[3];
+    $birthDate .= $temp;
+    $birthDate .= "-";
+    $birthDate .= "20";
+
+    for ($i = 0; $i < 2; $i++) {
+        $temp = (string) $arr[$i];
+        $birthDate .= $temp;
+    }
+
+    return $birthDate;
+}
+
+$psl = peselBirthDate("03221808729");
+echo $psl;
+
+echo "<br />";
+echo "Zadanie 5";
+echo "<br />";
+
+function areaCalculator($figure, $par1, $par2, $par3){
+    switch ($figure) {
+        case "triangle": triangleArea($par1, $par2); break;
+        case "rectangle": rectangleArea($par1, $par2); break;
+        case "trapezoid": trapezoidArea($par1, $par2, $par3); break;
+    }
+}
+
+function triangleArea($height, $base) {
+    echo (1/2) * $height * $base;
+}
+
+function rectangleArea($width, $height) {
+    echo $width * $height;
+}
+
+function trapezoidArea($longBase, $shortBase, $height) {
+    echo (1/2) * ($longBase + $shortBase) * $height;
+}
+
+areaCalculator("trapezoid", 1, 2, 3);
+
+
---
+++ zadania3/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//Zadanie 1
+
+echo "Zadanie 1";
+echo "<br />";
+
+function dice () {
+    $wynik = rand(1, 6);
+    echo $wynik;
+}
+
+dice();
+
+//Zadanie 2
+
+echo "Zadanie 2";
+echo "<br />";
+
